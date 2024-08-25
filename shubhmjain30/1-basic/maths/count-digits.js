f6099fc72832731f8e37dfb48b8e3b4d14ce0432
shubhmjain30 <shubhmjain30@gmail.com>
2024-08-25 23:51:59 +0530

    doc: count digit problem and example along with link

--- shubhmjain30/1-basic/maths/count-digits.js
+++ shubhmjain30/1-basic/maths/count-digits.js
@@ -1,4 +1,17 @@
-// question: https://takeuforward.org/data-structure/count-digits-in-a-number/
+/**
+ * Problem Statement: Given an integer N, return the number of digits in N.
+ *
+ * Example 1:
+ * Input:N = 12345
+ * Output:5
+ * Explanation:  The number 12345 has 5 digits.
+ * Example 2:
+ * Input:N = 7789
+ * Output: 4
+ * Explanation: The number 7789 has 4 digits.
+ *
+ * Link: https://takeuforward.org/data-structure/count-digits-in-a-number/
+ */
 
 let count_digits_1 = (num) => {
 	let count = 0;
