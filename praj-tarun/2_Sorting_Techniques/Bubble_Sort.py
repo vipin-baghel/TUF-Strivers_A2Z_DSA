9318d7d9236ca94f530e78f15a21dc573668e165
praj-tarun <tarun.praj@outook.com>
2024-09-07 12:19:37 +0530

    optimization for bubble sort

--- praj-tarun/2_Sorting_Techniques/Bubble_Sort.py
+++ praj-tarun/2_Sorting_Techniques/Bubble_Sort.py
@@ -1,9 +1,15 @@
 def bubble_sort(arr):
     i = len(arr)-1
     while i>=0:
+        #optimization for sorted array
+        swap=0
         for j in range(i):
             if arr[j]>arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1],arr[j]
+                swap=1
+        if swap == 0:
+            #sorted array, for loop oran only once
+            break
         i-=1
     return arr
 
