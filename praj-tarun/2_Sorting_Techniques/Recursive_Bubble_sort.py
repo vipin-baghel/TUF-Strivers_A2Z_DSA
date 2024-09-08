629bc74b0b1b2bec59cc26d391818bed0755a534
praj-tarun <tarun.praj@outook.com>
2024-09-08 22:10:09 +0530

    recursive bubble , insertion sort

--- praj-tarun/2_Sorting_Techniques/Recursive_Bubble_sort.py
+++ praj-tarun/2_Sorting_Techniques/Recursive_Bubble_sort.py
@@ -1,2 +1,13 @@
-def recursive_bubble(arr):
-    return
+def recursive_bubble(arr,low,high):
+    if low>=high:
+        return arr
+    for i in range(low,high):
+        if arr[i]>arr[i+1]:
+            arr[i],arr[i+1]=arr[i+1],arr[i]
+    recursive_bubble(arr,low,high-1)
+
+arr = [11,4,1,3,4,2]
+
+recursive_bubble(arr,0,len(arr)-1)
+
+print(arr)
