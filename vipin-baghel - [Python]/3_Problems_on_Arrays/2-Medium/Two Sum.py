caba2532319e139ea56bfb847417e92c8f59235d
Vipin Baghel <vipinbaghel1999@gmail.com>
2024-09-14 20:39:43 +0530

    two sum

--- vipin-baghel - [Python]/3_Problems_on_Arrays/2-Medium/Two Sum.py
+++ vipin-baghel - [Python]/3_Problems_on_Arrays/2-Medium/Two Sum.py
@@ -4,17 +4,26 @@
 # result = [1, 3]
 
 
-def two_sum_solution(arr, target):
-    for i in range(len(arr) - 1):
-        for j in range(i, len(arr) - 1):
+def two_sum_solution(arr, target):  # O(N^2)
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
             if arr[i] + arr[j] == target:
                 return [i, j]
+
+
+def two_sum_solution_hashing(arr, target):  # O(N)
+    my_dict = {}
+    for i in range(len(arr)):
+        var_complement = target - arr[i]
+        if var_complement not in my_dict.keys():
+            my_dict[var_complement] = i
+        else:
+            return [i, my_dict[var_complement]]
 
 
 if __name__ == "__main__":
     arr = [2, 6, 5, 8, 11]
     target = 14
-    arr2 = [2, 6, 5, 8, 11]
-    target2 = 15
+
     print(two_sum_solution(arr, target))
-    print(two_sum_solution(arr2, target2))
+    print(two_sum_solution(arr, target))
